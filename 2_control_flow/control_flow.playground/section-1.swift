cbfe8dd69953012eccf4f1a757654c81c6478676
Otavio <otamm@hotmail.com>
2015-01-09 13:15:25 -0200

    Initial commit for control flows

---
+++ 2_control_flow/control_flow.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let truckSpeed = 45
+
+let lamboSpeed = 120
+
+var mySpeed:Int //used when we don't wanna assign a value to that variable just now.
+
+mySpeed = truckSpeed
+
+if mySpeed < 70 {
+    println("Keep Cruising")
+} else {
+    println("Da polis will bust yo'ass lmfao")
+}
+// this control flow does what a control flow is supposed to: if "something" (comparison operator) "a value", execute "this code". For any other possibilities, execute "this other code".
