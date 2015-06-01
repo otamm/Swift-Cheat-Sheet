0a0aabfc9be09cbda564602d3935d4c6a2406e93
otamm <oivatom@gmail.com>
2015-06-01 15:50:10 -0300

    Finished structs

--- 4_structs/structs.playground/Contents.swift
+++ 4_structs/structs.playground/Contents.swift
@@ -7,23 +7,32 @@ import UIKit;
 struct Passport {
     var age = 0; // these are all default values.
     var name = "AAA"; // the final value for each instance is arbitrary.
-    var gender = "Hermaphrodite"; // the data type is established, though.
+    var gender:NSString = "Hermaphrodite"; // the data type is established, though.
     var citizenID = 0; // the 'citizenID' value cannot be a string, for example.
     var passportNum = ""; // neither 'passportNum' can be an int.
 }
 
 // This is just a set of arbitrary information that is glued together in a way that each collection of data with this structure (hah!) is unique and needs to be unique, the uniqueness being enforced by no two structs having the same information on every field.
 
+// instances are initialized with the default attributes defined in the struct.
 var johnny = Passport(); // an instance of the 'Passport' struct;
 johnny.age = 23;
 johnny.name = "Johnny Boy";
 johnny.citizenID = 9839128392832;
 johnny.passportNum = "3xZ74jKKP";
 
-johnny.gender = 8;//"Cannot assign a value of type 'Int' to a value of type 'String'
+johnny.gender = 8;//"Cannot assign a value of type 'Int' to a value of type 'NSString'."
 
 // since johnny's gender wasn't set, the default value "Hermaphrodite" will be used.
 var jGender = johnny.gender;
-jGender.stringVal("Hermaphrodite");
+jGender.isEqualToString("Hermaphrodite"); // returns true
 
 var jackie = Passport();
+jackie.age = 24;
+jackie.name = "Jackie Gurl";
+jackie.gender = "Female";
+jackie.citizenID = 382908239023;
+jackie.passportNum = "jZhMn89Uy673o";
+
+//Now, jackie is a struct all on its own, with every field containing individual, non-default values. 
+//These values are referenced to the struct instance and not the definition, like if a struct is an array/dictionary (structs are very similar to dictionaries, except all keys are pre-defined and unmutable); structs are copied each time an instance is initialized.
